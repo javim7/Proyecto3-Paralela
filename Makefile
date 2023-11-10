d2fae7b094355f8beffb720cbdcfeb2ef68fc1a6
Javier Mombiela <61723252+javim7@users.noreply.github.com>
2023-11-10 12:17:55 -0600

    version constante completada
    Co-Authored-By: IPablo271 <69815580+IPablo271@users.noreply.github.com>
    Co-Authored-By: Jose Andres Hernandez Guerra <her20053@uvg.edu.gt>

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
-all: pgm.o	houghBase
+all: pgm.o	houghBase2
 
-houghBase:	houghBase.cu pgm.o
-	nvcc houghBase.cu pgm.o -o houghBase
+houghBase2:	houghBase2.cu pgm.o
+	nvcc houghBase2.cu pgm.o -o houghBase2
 
 pgm.o:	pgm.cpp
 	g++ -c pgm.cpp -o ./pgm.o
--- houghBase2.cu
+++ houghBase2.cu
@@ -20,6 +20,11 @@ const int degreeInc = 2;
 const int degreeBins = 180 / degreeInc;
 const int rBins = 100;
 const float radInc = degreeInc * M_PI / 180;
+
+// Declaración de memoria constante
+__constant__ float d_Cos[degreeBins];
+__constant__ float d_Sin[degreeBins];
+
 //*****************************************************************
 // The CPU function returns a pointer to the accummulator
 void CPU_HoughTran (unsigned char *pic, int w, int h, int **acc)
@@ -71,7 +76,7 @@ void CPU_HoughTran (unsigned char *pic, int w, int h, int **acc)
 
 // GPU kernel. One thread per image pixel is spawned.
 // The accummulator memory needs to be allocated by the host in global memory
-__global__ void GPU_HoughTran (unsigned char *pic, int w, int h, int *acc, float rMax, float rScale, float *d_Cos, float *d_Sin)
+__global__ void GPU_HoughTran (unsigned char *pic, int w, int h, int *acc, float rMax, float rScale)
 {
   //DONE calcular: int gloID = 
   int gloID = blockIdx.x * blockDim.x + threadIdx.x;
@@ -126,7 +131,7 @@ int main (int argc, char **argv)
   cudaEvent_t start, stop;
   cudaEventCreate(&start);
   cudaEventCreate(&stop);
-
+  
   // CPU calculation
   CPU_HoughTran(inImg.pixels, w, h, &cpuht);
 
@@ -144,9 +149,9 @@ int main (int argc, char **argv)
   float rMax = sqrt (1.0 * w * w + 1.0 * h * h) / 2;
   float rScale = 2 * rMax / rBins;
 
-  // TODO eventualmente volver memoria global
-  cudaMemcpy(d_Cos, pcCos, sizeof (float) * degreeBins, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_Sin, pcSin, sizeof (float) * degreeBins, cudaMemcpyHostToDevice);
+  // DONE Copia de las constantes a la memoria constante
+  cudaMemcpyToSymbol(d_Cos, pcCos, sizeof(float) * degreeBins);
+  cudaMemcpyToSymbol(d_Sin, pcSin, sizeof(float) * degreeBins);
 
   // setup and copy data from host to device
   unsigned char *d_in, *h_in;
@@ -166,7 +171,7 @@ int main (int argc, char **argv)
   // execution configuration uses a 1-D grid of 1-D blocks, each made of 256 threads
   //1 thread por pixel
   int blockNum = ceil (w * h / 256);
-  GPU_HoughTran <<< blockNum, 256 >>> (d_in, w, h, d_hough, rMax, rScale, d_Cos, d_Sin);
+  GPU_HoughTran <<< blockNum, 256 >>> (d_in, w, h, d_hough, rMax, rScale);
 
   cudaEventRecord(stop); //registrar tiempo de fin
   cudaEventSynchronize(stop);
